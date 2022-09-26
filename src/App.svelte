a5c72c25a5d79814b346ed480e3c00b7bccf5faa
roynulrohan-esdc <roynul.rr.rohan@hrsdc-rhdcc.gc.ca>
2022-09-26 10:57:37 -0400

    Added support for new letter

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,49 @@
 <script lang="ts">
-  import { filledPDF, openTemplateFile, populatePDF } from "./stores/pdf";
-
-  openTemplateFile();
+  import { documentSelection, filledPDF, openTemplateFile, populatePDF } from "./stores/pdf";
 
   const testData = {
-    firstName: "John",
-    lastName: "Smith",
-    date: "11/11/1111",
-    pageNumbers: "1",
-    workType: "Benefits",
-    comments: "Some comments",
-    caseNumber: "0001",
-    pri: "123456789",
-    department: "Employment and Social Development Canada",
+    "PSPC-SPAC-446-3E": {
+      employeeFirstName: "John",
+      employeeLastName: "Smith",
+      employeePRI: "123456789",
+      workType: "Benefits",
+      department: "Employment and Social Development Canada",
+      caseNumber: "0001",
+      comments: "Some comments",
+      pageNumbers: "1",
+      date: "11/11/1111",
+    },
+    "PSPC-SPAC-446-5E": {
+      employeeFirstName: "John",
+      employeeLastName: "Smith",
+      employeePRI: "123456789",
+      employeeEmail: "johnsmith@email.com",
+      employeeTelephone: "1111111",
+      employeeAreaCode: "613",
+      workType: "Benefits",
+      department: "Employment and Social Development Canada",
+      caseNumber: "0001",
+      comments: "Some comments",
+      // subType: "",
+      effectiveStartDate: "11/11/1111",
+      requestorName: "Alice",
+      requestorEmail: "alice@email.com",
+      requestorAreaCode: "343",
+      requestorTelephone: "1111111",
+      trustedSourceName: "Bob",
+      trustedSourceEmail: "bob@email.com",
+      trustedSourceAreaCode: "416",
+      trustedSourceTelephone: "1111111",
+      pageNumbers: "1",
+      date: "11/11/1111",
+    },
   };
 
-  populatePDF(testData);
+  $: {
+    if ($documentSelection) {
+      openTemplateFile();
+    }
+  }
 </script>
 
 <main>
@@ -25,44 +53,57 @@
     </div>
 
     <div>
-      <h3>Example Data</h3>
-      <table class="table table-condensed mrgn-tp-lg">
-        <thead>
-          <tr>
-            <th scope="col" class="col-sm-2">Key</th>
-            <th scope="col" class="col-sm-6">Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each Object.keys(testData) as key}
-            <tr>
-              <td>
-                <p>
-                  {key}
-                </p>
-              </td>
-              <td>
-                <p>
-                  {testData[key]}
-                </p>
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-
-      <div>
-        <button
-          class="btn btn-primary mrgn-rght-md"
-          on:click={() => {
-            populatePDF(testData);
-          }}>Generate PDF</button
-        >
-        {#if $filledPDF}
-          <a href={$filledPDF} download="Filled.pdf"> Download </a>
-        {/if}
+      <h3>Document</h3>
+      <div class="form-group mrgn-tp-md">
+        <select id="documentSelection" class="form-control" bind:value={$documentSelection}>
+          <option label="Select a document" />
+          <option value="PSPC-SPAC-446-3E">PSPC-SPAC-446-3E</option>
+          <option value="PSPC-SPAC-446-5E">PSPC-SPAC-446-5E</option>
+        </select>
       </div>
     </div>
+
+    {#if $documentSelection}
+      <div class="mrgn-tp-lg">
+        <h3>Example Data</h3>
+        <table class="table table-condensed mrgn-tp-lg">
+          <thead>
+            <tr>
+              <th scope="col" class="col-sm-2">Key</th>
+              <th scope="col" class="col-sm-6">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each Object.keys(testData[$documentSelection]) as key}
+              <tr>
+                <td>
+                  <small>
+                    {key}
+                  </small>
+                </td>
+                <td>
+                  <small>
+                    {testData[$documentSelection][key]}
+                  </small>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+
+        <div>
+          <button
+            class="btn btn-primary mrgn-rght-md"
+            on:click={() => {
+              populatePDF(testData[$documentSelection]);
+            }}>Generate PDF</button
+          >
+          {#if $filledPDF}
+            <a href={$filledPDF} download="Filled.pdf" class="btn btn-success"> Download </a>
+          {/if}
+        </div>
+      </div>
+    {/if}
 
     {#if $filledPDF}
       <div class="pdf-container mrgn-tp-lg">
--- src/stores/pdf.ts
+++ src/stores/pdf.ts
@@ -1,24 +1,53 @@
 import { PDFDocument } from "pdf-lib";
 import { get, writable } from "svelte/store";
 
+export const documentSelection = writable<string>();
+export const filledPDF = writable<string>();
+
 const pdfDoc = writable<PDFDocument>();
-export const filledPDF = writable<any>();
 
 const pdfFieldNames = {
-  firstName: "PWGSC-TPSGC446-3E[0].Page1[0].FirstNme[0]",
-  lastName: "PWGSC-TPSGC446-3E[0].Page1[0].LastName[0]",
-  pageNumbers: "PWGSC-TPSGC446-3E[0].Page1[0].sfHeader[0].PageNumberOne[0]",
-  date: "PWGSC-TPSGC446-3E[0].Page1[0].sfHeader[0].ActionRequestDate[0]",
-  workType: "PWGSC-TPSGC446-3E[0].Page1[0].WorkTypeList[0]",
-  comments: "PWGSC-TPSGC446-3E[0].Page1[0].Comments[0]",
-  caseNumber: "PWGSC-TPSGC446-3E[0].Page1[0].CaseNumber[0]",
-  pri: "PWGSC-TPSGC446-3E[0].Page1[0].PRI[0]",
-  barcode: "PWGSC-TPSGC446-3E[0].Page1[0].Code3of9BarCode1[0]",
-  department: "PWGSC-TPSGC446-3E[0].Page1[0].DropDownList1[0]",
+  "PSPC-SPAC-446-3E": {
+    employeeFirstName: "PWGSC-TPSGC446-3E[0].Page1[0].FirstNme[0]",
+    employeeLastName: "PWGSC-TPSGC446-3E[0].Page1[0].LastName[0]",
+    employeePRI: "PWGSC-TPSGC446-3E[0].Page1[0].PRI[0]",
+    pageNumbers: "PWGSC-TPSGC446-3E[0].Page1[0].sfHeader[0].PageNumberOne[0]",
+    date: "PWGSC-TPSGC446-3E[0].Page1[0].sfHeader[0].ActionRequestDate[0]",
+    workType: "PWGSC-TPSGC446-3E[0].Page1[0].WorkTypeList[0]",
+    comments: "PWGSC-TPSGC446-3E[0].Page1[0].Comments[0]",
+    caseNumber: "PWGSC-TPSGC446-3E[0].Page1[0].CaseNumber[0]",
+    barcode: "PWGSC-TPSGC446-3E[0].Page1[0].Code3of9BarCode1[0]",
+    department: "PWGSC-TPSGC446-3E[0].Page1[0].DropDownList1[0]",
+  },
+  "PSPC-SPAC-446-5E": {
+    employeeFirstName: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeFirstNme[0]",
+    employeeLastName: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeLastName[0]",
+    employeeEmail: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeEmailAddress[0]",
+    employeeTelephone: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeTelephoneNumber[0]",
+    employeeAreaCode: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeAreaCode[0]",
+    employeePRI: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeePRI[0]",
+    pageNumbers: "PWGSC-TPSGC446-1E[0].Page1[0].sfHeader[0].PageNumberOne[0]",
+    date: "PWGSC-TPSGC446-1E[0].Page1[0].sfHeader[0].ActionRequestDate[0]",
+    workType: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].WorkTypeList[0]",
+    comments: "PWGSC-TPSGC446-1E[0].Page1[0].Comments[0]",
+    caseNumber: "PWGSC-TPSGC446-1E[0].Page1[0].EmployeeCaseNumber[0]",
+    department: "PWGSC-TPSGC446-1E[0].Page1[0].DepartmentList[0]",
+    subType: "PWGSC-TPSGC446-1E[0].Page1[0].#subform[2].SubTypeList[0]",
+    effectiveStartDate: "PWGSC-TPSGC446-1E[0].Page1[0].ActionRequestDate[0]",
+    requestorName: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].RequestorName[0]",
+    requestorEmail: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].RequestorEmailAddress[0]",
+    requestorAreaCode: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].RequestorAreaCode[0]",
+    requestorTelephone: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].RequestorTelephoneNumber[0]",
+    trustedSourceName: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].TrustedSourceName[0]",
+    trustedSourceEmail: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].TrustedSourceEmailAddress[0]",
+    trustedSourceAreaCode: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].TrustedSourceAreaCode[0]",
+    trustedSourceTelephone: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].TrustedSourceTelephoneNumber[0]",
+    barcode: "PWGSC-TPSGC446-1E[0].Page1[0].sfFooter[0].Code3of9BarCode1[0]",
+  },
 };
 
 export const openTemplateFile = async () => {
-  const existingPdfBytes = await fetch("./Template.pdf").then((res) => res.arrayBuffer());
+  const existingPdfBytes = await fetch(`./templates/${get(documentSelection)}.pdf`).then((res) => res.arrayBuffer());
 
   const pdf = await PDFDocument.load(existingPdfBytes);
 
@@ -36,23 +65,21 @@ export const populatePDF = async (data) => {
 
   const keys = Object.keys(data);
 
+  const fieldNames = pdfFieldNames[get(documentSelection)];
+
   keys.forEach((key) => {
-    if (key !== "department" && key !== "workType") {
-      const textField = form.getTextField(pdfFieldNames[key]);
+    if (key !== "department" && key !== "workType" && key !== "subType") {
+      const textField = form.getTextField(fieldNames[key]);
 
       textField.setText(data[key]);
     } else {
-      const dropdown = form.getDropdown(pdfFieldNames[key]);
+      const dropdown = form.getDropdown(fieldNames[key]);
 
       dropdown.select(data[key]);
     }
   });
 
-  const pdfBytes = await pdf.save();
+  const pdfBytes = await pdf.saveAsBase64({ dataUri: true, updateFieldAppearances: true });
 
-  const blob = new Blob([pdfBytes], {
-    type: "application/pdf",
-  });
-
-  filledPDF.set(URL.createObjectURL(blob));
+  filledPDF.set(pdfBytes);
 };
